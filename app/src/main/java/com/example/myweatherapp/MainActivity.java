9d3e520e648e29b73a3f1ddf83aae823a2a16de1
Alexander Jocobi <vazquez_alex@outlook.com>
2021-09-04 21:17:33 -0700

    ADD longitud y latitude

--- app/src/main/java/com/example/myweatherapp/MainActivity.java
+++ app/src/main/java/com/example/myweatherapp/MainActivity.java
@@ -1,15 +1,24 @@
 package com.example.myweatherapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
 import android.content.IntentSender;
 import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Looper;
+import android.provider.Settings;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.Request;
@@ -19,11 +28,15 @@ import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.common.api.ResolvableApiException;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.location.LocationSettingsRequest;
 import com.google.android.gms.location.LocationSettingsResponse;
 import com.google.android.gms.location.LocationSettingsStatusCodes;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.gun0912.tedpermission.PermissionListener;
 import com.gun0912.tedpermission.normal.TedPermission;
@@ -37,22 +50,43 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    FusedLocationProviderClient fusedLocationProviderClient;
     int REQUEST_LOCATION = 88;
 
     EditText txtUser, txtTitle, txtBody;
-    Button btnEnviar;
+    TextView tv_latitude, tv_longitude;
+    Button btnEnviar, btnlocation,button;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Verificamos si los servicios estan activos
+        checkPermissions();
+
+        //Asignacion de variables
         txtUser = findViewById(R.id.txtUser);
         txtTitle = findViewById(R.id.txtTitle);
         txtBody = findViewById(R.id.txtBody);
         btnEnviar = findViewById(R.id.btnEnviar);
+        tv_latitude = findViewById(R.id.tv_latitude);
+        tv_longitude = findViewById(R.id.tv_longitude);
+        btnlocation = findViewById(R.id.btnlocation);
+        button = findViewById(R.id.location);
 
-        getLocation();
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(MainActivity.this);
 
+        btnlocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getCurrentLocation();
+            }
+        });
+
+        /*
         btnEnviar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -60,25 +94,65 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        Button button = findViewById(R.id.location);
         button.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
-                //verificamos los permisos
-                if(ActivityCompat.checkSelfPermission(MainActivity.this
-                        ,Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
-                {
-                    //si el permiso es verdadero
-                    Location();
-                }else{
-                    //si el permiso no es falso
-                    ActivityCompat.requestPermissions(MainActivity.this
-                    , new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 23);
-                }
+                checkPermission();
             }
         });
+         */
     }
 
+    @SuppressLint("MissingPermission")
+    private void getCurrentLocation(){
+        //inicializamos el manager de localizacion
+        LocationManager locationManager = (LocationManager) getSystemService(
+                Context.LOCATION_SERVICE
+        );
+        if(locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+        || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)){
+            //Si el servicio esta activo
+            //Obtenemos la ultima locacion
+            fusedLocationProviderClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
+                @Override
+                public void onComplete(@NonNull Task<Location> task) {
+                    //inicializamos la locacion
+                    Location location = task.getResult();
+                    if(location != null){
+                        //Si no es nula la obtenemos
+                        tv_latitude.setText(String.valueOf(location.getLatitude()));
+                        tv_longitude.setText(String.valueOf(location.getLongitude()));
+                    }else{
+                        //Si es nula inicilizamos una peticion
+                        LocationRequest locationRequest = LocationRequest.create();
+                        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+                        locationRequest.setInterval(10000);
+                        locationRequest.setFastestInterval(1000);
+                        locationRequest.setNumUpdates(1);
+
+                        LocationCallback locationCallback = new LocationCallback() {
+                            @Override
+                            public void onLocationResult(LocationResult locationResult) {
+                                Location location1 = locationResult.getLastLocation();
+                                //set latitude
+                                tv_latitude.setText(String.valueOf(location1.getLatitude()));
+                                //set longitude
+                                tv_longitude.setText(String.valueOf(location1.getLongitude()));
+
+                            }
+                        };
+                        //Request location update
+                        fusedLocationProviderClient.requestLocationUpdates(locationRequest
+                                ,locationCallback
+                                , Looper.myLooper());
+                    }
+                }
+            });
+        }else{
+            //Cuando el servicio no esta activado
+            checkPermissions();
+        }
+    }
     private void LeerWS(){
         String url = "https://jsonplaceholder.typicode.com/posts/1";
 
@@ -103,7 +177,26 @@ public class MainActivity extends AppCompatActivity {
         Volley.newRequestQueue(this).add(postRequest);
     }
 
-    private void Location() {
+    //Da permisos a la APP y activa GPS
+    private void checkPermissions() {
+        PermissionListener permissionlistener = new PermissionListener() {
+            @Override
+            public void onPermissionGranted() {
+                Toast.makeText(MainActivity.this, "Permisos Consedidos", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onPermissionDenied(List<String> deniedPermissions) {
+                Toast.makeText(MainActivity.this, "Permisos Denegados\n" + deniedPermissions.toString(), Toast.LENGTH_LONG).show();
+            }
+        };
+
+        TedPermission.create()
+                .setPermissionListener(permissionlistener)
+                .setDeniedMessage("Si no aceptas los permisos,no podras utilizar la APP\n\nPor favor cambia los permisos en  [Setting] > [Permission]")
+                .setPermissions(Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION)
+                .check();
+
         LocationRequest request = LocationRequest.create();
         request.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
         request.setInterval(5000);
@@ -124,7 +217,6 @@ public class MainActivity extends AppCompatActivity {
                 switch (e.getStatusCode()){
                     case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
                         break;
-
                     case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
                         try{
                             ResolvableApiException resolvableApiException = (ResolvableApiException) e;
@@ -133,31 +225,10 @@ public class MainActivity extends AppCompatActivity {
 
                         }
                         break;
-
                 }
                 e.printStackTrace();
             }
         });
 
-    }
-
-    private void getLocation() {
-        PermissionListener permissionlistener = new PermissionListener() {
-            @Override
-            public void onPermissionGranted() {
-                Toast.makeText(MainActivity.this, "Permisos Consedidos", Toast.LENGTH_SHORT).show();
-            }
-
-            @Override
-            public void onPermissionDenied(List<String> deniedPermissions) {
-                Toast.makeText(MainActivity.this, "Permisos Denegados\n" + deniedPermissions.toString(), Toast.LENGTH_SHORT).show();
-            }
-        };
-
-        TedPermission.create()
-                .setPermissionListener(permissionlistener)
-                .setDeniedMessage("Si no aceptas los permisos,no podras utilizar la APP\n\nPor favor cambia los permisos en  [Setting] > [Permission]")
-                .setPermissions(Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION)
-                .check();
     }
 }
